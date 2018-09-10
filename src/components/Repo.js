dcf960a0411a276740d3d3646c9d2ee7dcc537cd
lauradml <laura.domingo.82@gmail.com>
2018-09-10 17:28:26 +0200

    cambiado link al nombre

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 class Repo extends React.Component {
   render(){
       const{name, description, language}= this.props.item;
     return (
       <div>
-        <h2>{name}</h2>
+        <Link to={`/repoDetail/${this.props.item.id}`} >
+          <h2>{name}</h2>
+        </Link>
         <p>{description}</p>
         <p>{language}</p>
       </div>
